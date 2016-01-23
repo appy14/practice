2c6129b0c4da0ec06043ea1fcd875cce80498645
Aparna Swaminathan <aparnaswaminathan@Aparnas-MacBook-Pro.local>
2016-01-23 16:03:57 -0600

    Exercise 2.3 - Linkedlist

--- cracking_the_coding_interview/src/main/java/com/as/funexercises/ch2/LinkedList.java
+++ cracking_the_coding_interview/src/main/java/com/as/funexercises/ch2/LinkedList.java
@@ -3,21 +3,21 @@ package com.as.funexercises.ch2;
 import java.util.HashSet;
 
 public class LinkedList {
-    
+
     // Assuming an unsorted list
     public static LinkedListNode removeDuplicatesWithoutABuffer(LinkedListNode n) {
-        
+
         // considering n as the head, singly linked list
         if (n == null || n.next == null) {
             return n;
         }
-        
+
         LinkedListNode current = n.next;
         LinkedListNode prev = n;
-        
+
         while (current != null) {
             LinkedListNode runner = n;
-            
+
             while (runner != current) {
                 if (runner.data == current.data) {
                     prev.next = current.next;
@@ -26,7 +26,7 @@ public class LinkedList {
                 }
                 runner = runner.next;
             }
-            
+
             if (runner == current) {
                 prev = current;
                 current = current.next;
@@ -34,19 +34,19 @@ public class LinkedList {
         }
         return n;
     }
-    
+
     // Assuming an unsorted list
     public static LinkedListNode removeDuplicatesWithABuffer(LinkedListNode n) {
         if (n == null || n.next == null) {
             return n;
         }
-        
+
         HashSet<Integer> values = new HashSet<Integer>();
-        
+
         LinkedListNode current = n.next;
         LinkedListNode prev = n;
         values.add(n.data);
-        
+
         while (current != null) {
             if (values.contains(current.data)) {
                 prev.next = current.next;
@@ -59,31 +59,47 @@ public class LinkedList {
         }
         return n;
     }
-    
+
     public static LinkedListNode findNthLast(LinkedListNode head, int n) {
         if (head == null || n < 0) {
             return null;
         }
-        
+
         LinkedListNode current = head;
-        
+
         LinkedListNode pointer = head;
-        
+
         int count = 0;
-        
+
         while (count++ < n && pointer != null) {
             pointer = pointer.next;
         }
-        
+
         if (pointer == null) {
             return null;
         }
-        
+
         while (pointer.next != null) {
             current = current.next;
             pointer = pointer.next;
         }
-        
+
         return current;
+    }
+
+    // Ex 2.3 delete a node in the middle of a singly linked list,
+    // given only access to that node
+    public static boolean deleteMidNode(LinkedListNode n) {
+        if (n == null || n.next == null) {
+            return false;
+        }
+
+        while (n.next.next != null) {
+            n.data = n.next.data;
+            n = n.next;
+        }
+        n.data = n.next.data;
+        n.next = null;
+        return true;
     }
 }
--- cracking_the_coding_interview/src/test/java/com/as/funexercises/ch2/LinkedListTest.java
+++ cracking_the_coding_interview/src/test/java/com/as/funexercises/ch2/LinkedListTest.java
@@ -1,6 +1,7 @@
 package com.as.funexercises.ch2;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -13,34 +14,34 @@ public class LinkedListTest {
         LinkedListNode n3 = new LinkedListNode(10);
         LinkedListNode n4 = new LinkedListNode(30);
         LinkedListNode n5 = new LinkedListNode(20);
-        
+
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
-        
+
         LinkedList.removeDuplicatesWithoutABuffer(n1);
-        
+
         assertEquals(n2, n1.next);
         assertEquals(n4, n2.next);
         assertEquals(null, n4.next);
-        
+
         // No duplicates
         LinkedList.removeDuplicatesWithoutABuffer(n1);
         assertEquals(n2, n1.next);
         assertEquals(n4, n2.next);
         assertEquals(null, n4.next);
-        
+
         // There is only one node
         LinkedListNode n6 = new LinkedListNode(40);
         assertEquals(n6, LinkedList.removeDuplicatesWithoutABuffer(n6));
         assertEquals(null, n6.next);
-        
-        // Pass null 
+
+        // Pass null
         assertEquals(null, LinkedList.removeDuplicatesWithoutABuffer(null));
-        
+
     }
-    
+
     @Test
     public void testRemoveDuplicatesWithABuffer() {
         LinkedListNode n1 = new LinkedListNode(10);
@@ -48,33 +49,33 @@ public class LinkedListTest {
         LinkedListNode n3 = new LinkedListNode(10);
         LinkedListNode n4 = new LinkedListNode(30);
         LinkedListNode n5 = new LinkedListNode(20);
-        
+
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
-        
+
         LinkedList.removeDuplicatesWithABuffer(n1);
-        
+
         assertEquals(n2, n1.next);
         assertEquals(n4, n2.next);
         assertEquals(null, n4.next);
-        
+
         // No duplicates
         LinkedList.removeDuplicatesWithABuffer(n1);
         assertEquals(n2, n1.next);
         assertEquals(n4, n2.next);
         assertEquals(null, n4.next);
-        
+
         // There is only one node
         LinkedListNode n6 = new LinkedListNode(40);
         assertEquals(n6, LinkedList.removeDuplicatesWithABuffer(n6));
         assertEquals(null, n6.next);
-        
-        // Pass null 
+
+        // Pass null
         assertEquals(null, LinkedList.removeDuplicatesWithABuffer(null));
     }
-    
+
     @Test
     public void testFindNthLast() {
         LinkedListNode n1 = new LinkedListNode(10);
@@ -82,19 +83,42 @@ public class LinkedListTest {
         LinkedListNode n3 = new LinkedListNode(10);
         LinkedListNode n4 = new LinkedListNode(30);
         LinkedListNode n5 = new LinkedListNode(20);
-        
+
         n1.next = n2;
         n2.next = n3;
         n3.next = n4;
         n4.next = n5;
-        
+
         assertEquals(n5, LinkedList.findNthLast(n1, 0));
         assertEquals(n4, LinkedList.findNthLast(n1, 1));
         assertEquals(n3, LinkedList.findNthLast(n1, 2));
         assertEquals(n2, LinkedList.findNthLast(n1, 3));
         assertEquals(n1, LinkedList.findNthLast(n1, 4));
-        
+
         assertEquals(null, LinkedList.findNthLast(n1, 5));
         assertEquals(null, LinkedList.findNthLast(n1, -1));
+    }
+
+    @Test
+    public void testDeleteMidNode() {
+        LinkedListNode n1 = new LinkedListNode(10);
+        LinkedListNode n2 = new LinkedListNode(20);
+        LinkedListNode n3 = new LinkedListNode(10);
+        LinkedListNode n4 = new LinkedListNode(30);
+        LinkedListNode n5 = new LinkedListNode(20);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        assertEquals(true, LinkedList.deleteMidNode(n3));
+        assertEquals(20, n2.data);
+        assertEquals(30, n3.data);
+        assertEquals(20, n4.data);
+        assertEquals(null, n4.next);
+
+        assertFalse(LinkedList.deleteMidNode(n4));
+        assertFalse(LinkedList.deleteMidNode(null));
     }
 }
